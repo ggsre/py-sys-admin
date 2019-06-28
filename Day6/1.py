22b8dded0de92bccf64ee07b9526058562c27cdc
ggsre <49792118+ggsre@users.noreply.github.com>
2019-06-28 20:32:58 +0530

    Create 1.py

---
+++ Day6/1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import textwrap
+
+sample_text = '''
+        The textwrap module can be used to format text for output in situations
+        where pretty-printing is desired.  It offers programmatic functionality similar
+        to the paragraph wrapping or filling features found in many text editors.
+        '''
+
+s = '''
+# Filling Paragraphs
+print 'No dedent:\n'
+print textwrap.fill(sample_text)
+'''
+
+s = '''
+# Removing Existing Indentation
+dedented_text = textwrap.dedent(sample_text).strip()
+print
+print 'Dedented:\n'
+print dedented_text
+'''
+
+s = '''
+# Combining Dedent and Fill
+dedented_text = textwrap.dedent(sample_text).strip()
+for width in [ 20, 60, 80 ]:
+  print
+  print '%d Columns:\n' % width
+  print textwrap.fill(dedented_text, width=width)
+'''
+
+# Hanging Indents
+dedented_text = textwrap.dedent(sample_text).strip()
+print textwrap.fill(dedented_text, initial_indent='', subsequent_indent='    ')
