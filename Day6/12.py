30b3ec7e1afc0fa14b848caa8232ea5d06afdbcb
ggsre <49792118+ggsre@users.noreply.github.com>
2019-06-28 21:08:57 +0530

    Create 12.py

---
+++ Day6/12.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#
+# Character Sets - Ranges
+#
+from re_test_patterns import test_patterns
+
+test_patterns('This is some text -- with punctuation.',
+              [ '[a-z]+',      # sequences of lower case letters
+                '[A-Z]+',      # sequences of upper case letters
+                '[a-zA-Z]+',   # sequences of lower or upper case letters
+                '[A-Z][a-z]+', # one upper case letter followed by lower case letters
+                ])
